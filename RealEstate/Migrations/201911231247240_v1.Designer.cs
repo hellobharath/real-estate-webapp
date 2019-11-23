3ce2b4dc1c2774244d6b96a88851600edef0e5a2
hellobharath <bharathshn331@gmail.com>
2019-11-23 19:20:37 +0530

    db changes updated

--- RealEstate/Migrations/201911231247240_v1.Designer.cs
+++ RealEstate/Migrations/201911231247240_v1.Designer.cs
@@ -7,13 +7,13 @@ namespace RealEstate.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class v2 : IMigrationMetadata
+    public sealed partial class v1 : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(v2));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(v1));
         
         string IMigrationMetadata.Id
         {
-            get { return "201911220815564_v2"; }
+            get { return "201911231247240_v1"; }
         }
         
         string IMigrationMetadata.Source
--- RealEstate/Migrations/201911231247240_v1.cs
+++ RealEstate/Migrations/201911231247240_v1.cs
@@ -59,6 +59,22 @@ namespace RealEstate.Migrations
                 .Index(t => t.UserId);
             
             CreateTable(
+                "dbo.Contact",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Phno = c.Int(),
+                        Address = c.String(maxLength: 50),
+                        Pin = c.Int(),
+                        City = c.String(maxLength: 50),
+                        State = c.String(maxLength: 50),
+                        Email = c.String(maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.Id)
+                .Index(t => t.Id);
+            
+            CreateTable(
                 "dbo.AspNetUserLogins",
                 c => new
                     {
@@ -83,12 +99,15 @@ namespace RealEstate.Migrations
                         Payer_Id = c.String(maxLength: 128),
                         Payee_Id = c.String(maxLength: 128),
                         Status = c.String(maxLength: 50),
+                        Property_Id = c.Int(nullable: false),
                     })
                 .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Property", t => t.Property_Id, cascadeDelete: true)
                 .ForeignKey("dbo.AspNetUsers", t => t.Payee_Id)
                 .ForeignKey("dbo.AspNetUsers", t => t.Payer_Id)
                 .Index(t => t.Payer_Id)
-                .Index(t => t.Payee_Id);
+                .Index(t => t.Payee_Id)
+                .Index(t => t.Property_Id);
             
             CreateTable(
                 "dbo.Property",
@@ -97,6 +116,7 @@ namespace RealEstate.Migrations
                         Id = c.Int(nullable: false, identity: true),
                         Category = c.String(maxLength: 50),
                         Location = c.String(maxLength: 50),
+                        City = c.String(maxLength: 50),
                         Owner_Id = c.String(maxLength: 128),
                         Status = c.String(maxLength: 50),
                         image1 = c.Binary(),
@@ -153,22 +173,6 @@ namespace RealEstate.Migrations
                 .Index(t => t.RoleId);
             
             CreateTable(
-                "dbo.Contact",
-                c => new
-                    {
-                        Id = c.String(nullable: false, maxLength: 128),
-                        Phno = c.Int(),
-                        Address = c.String(maxLength: 50),
-                        Pin = c.Int(),
-                        City = c.String(maxLength: 50),
-                        State = c.String(maxLength: 50),
-                        Email = c.String(maxLength: 256),
-                    })
-                .PrimaryKey(t => t.Id)
-                .ForeignKey("dbo.AspNetUsers", t => t.Id)
-                .Index(t => t.Id);
-            
-            CreateTable(
                 "dbo.AspNetRoles",
                 c => new
                     {
@@ -183,39 +187,41 @@ namespace RealEstate.Migrations
         public override void Down()
         {
             DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
-            DropForeignKey("dbo.Contact", "Id", "dbo.AspNetUsers");
             DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Agreement", "Payer_Id", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Agreement", "Payee_Id", "dbo.AspNetUsers");
             DropForeignKey("dbo.Residential", "Id", "dbo.Property");
             DropForeignKey("dbo.Plot", "Id", "dbo.Property");
             DropForeignKey("dbo.Property", "Owner_Id", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Agreement", "Property_Id", "dbo.Property");
             DropForeignKey("dbo.Ad", "Property_Id", "dbo.Property");
-            DropForeignKey("dbo.Agreement", "Payer_Id", "dbo.AspNetUsers");
-            DropForeignKey("dbo.Agreement", "Payee_Id", "dbo.AspNetUsers");
             DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Contact", "Id", "dbo.AspNetUsers");
             DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
             DropForeignKey("dbo.Ad", "Owner_Id", "dbo.AspNetUsers");
             DropIndex("dbo.AspNetRoles", "RoleNameIndex");
-            DropIndex("dbo.Contact", new[] { "Id" });
             DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
             DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
             DropIndex("dbo.Residential", new[] { "Id" });
             DropIndex("dbo.Plot", new[] { "Id" });
             DropIndex("dbo.Property", new[] { "Owner_Id" });
+            DropIndex("dbo.Agreement", new[] { "Property_Id" });
             DropIndex("dbo.Agreement", new[] { "Payee_Id" });
             DropIndex("dbo.Agreement", new[] { "Payer_Id" });
             DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.Contact", new[] { "Id" });
             DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
             DropIndex("dbo.AspNetUsers", "UserNameIndex");
             DropIndex("dbo.Ad", new[] { "Owner_Id" });
             DropIndex("dbo.Ad", new[] { "Property_Id" });
             DropTable("dbo.AspNetRoles");
-            DropTable("dbo.Contact");
             DropTable("dbo.AspNetUserRoles");
             DropTable("dbo.Residential");
             DropTable("dbo.Plot");
             DropTable("dbo.Property");
             DropTable("dbo.Agreement");
             DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.Contact");
             DropTable("dbo.AspNetUserClaims");
             DropTable("dbo.AspNetUsers");
             DropTable("dbo.Ad");
